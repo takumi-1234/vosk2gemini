714bfd0ff85656dae84495c4cbd8c7f7c79ce72a
reina314 <hoge@hoge>
2024-09-14 16:55:47 +0900

    add: Command.py

---
+++ vosk2cmd/Command.py
@@ -0,0 +1,8 @@
+class Command:
+    GO_LEFT     = 'GO_LEFT'
+    GO_RIGHT    = 'GO_RIGHT'
+    GO_FORWARD  = 'GO_FORWARD'
+    GO_BACKWARD = 'GO_BACKWARD'
+    TURN_LEFT   = 'TURN_LEFT'
+    TURN_RIGHT  = 'TURN_RIGHT'
+    STOP        = 'STOP'
